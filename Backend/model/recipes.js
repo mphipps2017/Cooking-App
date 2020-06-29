a729d18533d85a417933c2b0026df3c1658585a1
mphipps2017 <mnphipps98@gmail.com>
2020-06-29 11:27:18 -0500

    Made a mock model

--- Backend/model/recipes.js
+++ Backend/model/recipes.js
@@ -1,5 +1,12 @@
-const recipes = {
 
-}
+// Title = ''
+// Ingedients = [{ingredient:'name', quantity:''}, ...]
+// Instructions = ['', '', .... , '']
+const recipes = [{
+    id: 1,
+    title: 'Sunny side up eggs',
+    ingedients: [{ingredient: 'egg', quantity:'1'},{ingredient:'butter', quantity:'20g'}],
+    instructions: ['Heat pan', 'put butter in', 'put egg and fry til somewhat cooked']
+}]
 
-modules.exports = recipes;
+module.exports = recipes;
--- Backend/routes/api/recipes.js
+++ Backend/routes/api/recipes.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
-module.exports = router;
+const recipeModel =  require('../../model/recipes');
 
 router.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send(recipeModel);
 });
 
 router.post('/', (req, res) => {
-    res.send(req.body);
-    // Title = ''
-    // Ingedients = []
-    // Instructions = ['', '', .... , '']
+    if(req.body.title !== '' && req.body.ingredients.length > 0 && req.body.instructions.length > 0){
+        const newRecipe = {
+            id: Math.random(),
+            title: req.body,
+            ingredients: req.body.ingredients,
+            instructions: req.body.instructions
+        };
+        recipeModel.push(newRecipe);
+        res.send(recipeModel);
+    } else {
+        res.status(400).json({msg:`A field of data has not been filled in`});
+    }
 });
 
+module.exports = router;
