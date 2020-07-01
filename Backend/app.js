69dfb0846fa4c145f3b4024d3a8ba2b9ae42afc8
mphipps2017 <mnphipps98@gmail.com>
2020-07-01 10:16:12 -0500

    Started setting up mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();``
 const PORT = process.env.PORT || 3000;
@@ -14,5 +15,11 @@ app.use(express.json());
 
 // Routes
 app.use('/api/recipes', require('./routes/api/recipes'));
+
+// Connect to DB
+// TODO install on mac, https://apple.stackexchange.com/questions/362883/mongodb-doesnt-work-after-update-to-macos-catalina-10-15
+mongoose.connect('mongodb://localhost:27017', () =>{
+  console.log('Connected to mongoDB!');
+});
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
