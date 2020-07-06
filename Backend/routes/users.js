ab0bd45359e009807778a3013323bafd91656b56
mphipps2017 <mnphipps98@gmail.com>
2020-07-06 13:45:48 -0500

    Continued setting up users route

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,5 +1,58 @@
-const express = require('express');
+const router = require('express').Router();
 const mongoose = require('mongoose');
-const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('../model/user');
+
+router.get('/login', (req, res) =>{
+    User.findOne({username: req.body.username}, (err, userInfo) =>{
+        if(err){
+            console.log(err);
+        } else {
+            console.log('HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEERRRRRRRREEEEEEEEEEEE');
+            if(bcrypt.compareSync(req.body.password, userInfo.password)){
+                res.status(200).json({msg:`Login success! Welcome ${userInfo.username}`}, userInfo);
+                // Use some kind of tokenizer for login validation here.
+            }
+        }
+    });
+});
+
+router.post('/register', (req, res) => {
+    if(req.body.email && req.body.username && req.body.password){
+        const newUser = new User({
+            _id: new mongoose.Types.ObjectId,
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email
+        });
+        newUser.save((err) =>{
+            if(err){
+                console.log(err);
+            } else{
+                res.status(200).json({msg:'Success! A new user has been created.'});
+            }
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) =>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    newUser.password = hash;
+                    newUser.save((err) =>{
+                        if(err){
+                            console.log(err);
+                        } else{
+                            res.status(200).json({msg:'Success! A new user has been created.'});
+                        }
+                    });
+                }
+            });
+        });
+    } else {
+        res.status(400).json({msg:`A field of data has not been filled in`});
+    }
+});
 
 module.exports = router;
