9f4d191cbb23b04f5bfd08a6805040738ed5175e
mphipps2017 <mnphipps98@gmail.com>
2020-07-21 16:58:36 -0500

    Created a base navbar and universal color scheme

--- Frontend/react-app/src/App.js
+++ Frontend/react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import SignInBox from './components/pages/SignInBox';
+import Navbar from './components/navbars/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <SignInBox />
     </div>
   );
---
+++ Frontend/react-app/src/components/navbars/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Styles from './Navbar.module.css';
+// Probably will need to replace all the hrefs with this in future
+// import {Link, withRouter} from 'react-router-dom';
+// TODO proptypes
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div className={Styles.wrapper}>
+                <div className={Styles.leftItems}>
+                    <a href="a link">About</a>
+                    <a href="a link">F.A.Q</a>
+                </div>
+
+                <div className="rightLinks">
+                    <a href="a link">Sign-in</a>
+                    <a href="a link">Create Account</a>
+                </div>
+            </div>
+        );
+        
+    }
+}
+
+export default Navbar;
